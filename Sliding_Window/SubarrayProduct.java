8c4e35eca9d61215d5df24c6421228ee7f14bf82
rakeshraj mahakud <rakeshraj.mahakud03@gmail.com>
2022-03-24 00:16:12 +0530

    second commit

--- Sliding_Window/SubarrayProduct.java
+++ Sliding_Window/SubarrayProduct.java
@@ -5,25 +5,8 @@ import java.util.Deque;
 
 public class SubarrayProduct {
     public static void main(String[] args) {
-        int[] nums = {1,5,2,3,6,4};
+        int[] arr= {1,5,2,3,6,4};
         int k = 4;
-        int l=nums.length, i=0, j=0, t=0;
-        int res= 1;
-        Deque<Integer> dq=new ArrayDeque<Integer>();
-        while(j<l){
-            while(!dq.isEmpty() && dq.peekLast()<nums[j]){
-                dq.removeLast();
-            }
-            dq.add(nums[j]);
-            if(j-i+1<k)
-                j++;
-            else if(j-i+1==k){
-                res *=dq.peek();
-                if(dq.peek()==nums[i])  dq.remove();
-                i++;
-                j++;
-            }
-        }
-        System.out.print(res);
+
     }
 }
