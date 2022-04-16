4568287bac3a8627ed6fde783c1a711d42d4baff
rakeshraj mahakud <rakeshraj.mahakud03@gmail.com>
2022-04-16 20:25:26 +0530

    16/04/2022

---
+++ String/Check.java
@@ -0,0 +1,11 @@
+package String;
+
+public class Check {
+    public static void main(String[] args) {
+        String s = new String("Java");
+        String s1 = new String("Java");
+        System.out.println(s==s1);
+        System.out.println(s.equals(s1));
+
+    }
+}
